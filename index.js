379775966bdb1ccccf42589e1105eef06d25a4de
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2021-11-12 12:00:09 -0300

    EXP: added experiment with parameters

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const app = express();
 
 app.use(express.static(path.resolve(__dirname, 'public')));
 
+app.get('/user', (req, res)=>{
+  res.status(200).send(req.query);
+});
+
 app.listen(8080, 'localhost',()=> {
   console.log("Servidor está ouvindo na porta 8080");
 });
