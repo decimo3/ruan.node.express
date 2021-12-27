5b52e8c791cae73002e8fd059de0736f47353d09
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2021-12-26 21:12:59 -0300

    DONE: static file web server

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static(path.resolve(__dirname, 'public')))
+
+app.listen(8080, 'localhost',()=> {
+  console.log("Servidor está ouvindo na porta 8080");
+});
