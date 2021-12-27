4391c8753a0707178a1cd5dd828aa307dd4499ef
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2021-12-26 21:15:55 -0300

    FIX: forgotten a semicolon

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(path.resolve(__dirname, 'public')))
+app.use(express.static(path.resolve(__dirname, 'public')));
 
 app.listen(8080, 'localhost',()=> {
   console.log("Servidor está ouvindo na porta 8080");
