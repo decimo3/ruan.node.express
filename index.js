3e355a375a0e0deb3aa9abbb09f1f5d9f2e8d7e3
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2021-12-28 21:22:22 -0300

    ADDED: protected route for testing

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const path = require('path');
+const jwt = require('express-jwt');
+require('dotenv').config()
 
 const app = express();
 
@@ -9,6 +11,13 @@ app.get('/user', (req, res)=>{
   res.status(200).send(req.query);
 });
 
-app.listen(8080, 'localhost',()=> {
+app.get('/protect', jwt({
+  secret: process.env.SECRET_KEY,
+  algorithms: [process.env.ALGORITHM]
+}),(req, res)=>{
+  res.status(200).send(req.query);
+});
+
+app.listen(process.env.PORT, 'localhost',()=> {
   console.log("Servidor está ouvindo na porta 8080");
 });
